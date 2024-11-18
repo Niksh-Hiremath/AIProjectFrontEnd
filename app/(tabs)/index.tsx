3bd89cc4ebbe6b972b7c1e005ff5768e02125d83
Armaan <raisinghani.armaan1@gmail.com>
2024-11-19 00:47:55 +0530

    bad code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,89 @@
-import React from "react";
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import { useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Button,
+  StyleSheet,
+} from "react-native";
 import {
   VideoCameraIcon,
   VideoCameraSlashIcon,
 } from "react-native-heroicons/outline";
 import { useState } from "react";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import React from "react";
 
 const App: React.FC = () => {
   const [active, setActive] = useState(false);
-
+  const [camera, setCamera] = useState(null);
+  const [record, setRecord] = useState(null);
+  const [recording, setRecording] = useState(false);
+  const onCameraReady = () => {
+    setRecording(true);
+  };
+  useEffect(() => {
+    (async () => {
+      if (recording && camera) {
+        console.log("Recording");
+        const localRecord = camera.record();
+        console.log(localRecord)
+        localRecord.then((r) => {
+          console.log(r);
+          setRecord(r.uri);
+          console.log(record);
+        });
+      }
+    })();
+  }, [recording, camera]);
+  const stopVideo = async () => {
+    console.log("Stopping video");
+    await camera.stopRecording();
+  };
   function handlePress() {
+    const t = !active;
     setActive(!active);
-    if (active) {
-      // Make API call to backend to start
+
+    if (t) {
       console.log("Camera is active");
+
+      setRecording(true);
     } else {
-      // Make API call to backend to stop
       console.log("Camera is inactive");
+      stopVideo();
+      setRecording(false);
+      setCamera(null);
     }
   }
 
+  const [permission, requestPermission] = useCameraPermissions();
+  if (!permission) {
+    return <View />;
+  }
   return (
-    <View className="bg-primary p-10 rounded-3xl m-10 flex-auto flex-col justify-between items-center content-center gap-10">
+    <ScrollView
+      className={
+        "bg-primary p-10 rounded-3xl m-10 flex-auto flex-col content-center gap-10 box-content " +
+        (active ? "border-red-600 border-solid border-8 p-8" : "")
+      }
+    >
+      {active && permission.granted ? (
+        <CameraView
+          style={[styles.camera]}
+          facing="front"
+          ref={(ca) => setCamera(ca)}
+          onCameraReady={onCameraReady}
+        ></CameraView>
+      ) : (
+        <></>
+      )}
+      {!permission && (
+        <View>
+          <Text>We need your permission to show the camera</Text>
+          <Button onPress={requestPermission} title="grant permission" />
+        </View>
+      )}
       {/* Main Content Container */}
       <View className="bg-white rounded-3xl h-4/5">
         <ScrollView>
@@ -29,44 +91,39 @@ const App: React.FC = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. 
-            {"\n"} Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.
-            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat.
-            {"\n"} Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.
-            {"\n"} Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.
-            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat.
-            {"\n"} Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.
-            {"\n"} Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.
-            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat.
+            aliquip ex ea commodo consequat.
+            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat.
+            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat.
+            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat.
+            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat.
+            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat.
+            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat.
+            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat.
+            {"\n"} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat.
           </Text>
         </ScrollView>
       </View>
@@ -85,8 +142,16 @@ const App: React.FC = () => {
           />
         )}
       </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 };
 
 export default App;
+const styles = StyleSheet.create({
+  camera: {
+    height: 300,
+    width: 300,
+    borderColor: "red",
+    borderWidth: 2,
+  },
+});
